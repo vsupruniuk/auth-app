47b610ca90f1f68998ee6c7d5b4629fee3fa2fe6
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2023-01-30 11:31:47 +0200

    Test deploy

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,11 +2,4 @@
 
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  logging: false,
-});
+export const sequelize = new Sequelize(process.env.DB_LINK)
